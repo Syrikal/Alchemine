2687be3cb9a67b570aa0ebc1361657739a33ed75
Syric <76715986+Syrikal@users.noreply.github.com>
2022-06-23 18:05:56 -0400

    Added slime enhancers and crafting recipes. Also the basic Slimeseed. Also all the models, blockstates,and textures for chromaslime and lumaslime.

---
+++ src/main/java/syric/alchemine/outputs/items/SlimeseedItem.java
@@ -0,0 +1,52 @@
+package syric.alchemine.outputs.items;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+public class SlimeseedItem extends Item {
+    public static final String DATA_ENTITY = "SlimeseedData";
+
+    public SlimeseedItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    @SuppressWarnings("ConstantConditions")
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        Slime entity = EntityType.SLIME.create(level);
+        BlockPos pos = context.getClickedPos().relative(context.getClickedFace());
+        ItemStack stack = context.getItemInHand();
+        Player player = context.getPlayer();
+
+        if (!level.isClientSide && stack.hasTag()) // read data first!: setting position before reading will reset that position!
+        {
+//            CompoundNBT tag = stack.getTag();
+//            if (tag.contains(DATA_ENTITY)) entity.deserializeNBT(tag.getCompound(DATA_ENTITY));
+            if (stack.hasCustomHoverName())
+                entity.setCustomName(stack.getHoverName()); // set entity name from stack name
+        }
+
+        entity.absMoveTo(context.getClickLocation().x, context.getClickLocation().y, context.getClickLocation().z);
+        if (!level.noCollision(entity)) {
+            return InteractionResult.FAIL;
+        }
+
+        if (!player.isCreative() || stack.getOrCreateTag().contains(DATA_ENTITY)) {
+            player.setItemInHand(context.getHand(), ItemStack.EMPTY);
+        }
+        entity.setDeltaMovement(Vec3.ZERO);
+        entity.yBodyRot = entity.yHeadRot = player.yHeadRot + 180;
+        level.addFreshEntity(entity);
+        return InteractionResult.SUCCESS;
+    }
+}
--- src/main/java/syric/alchemine/setup/AlchemineItems.java
+++ src/main/java/syric/alchemine/setup/AlchemineItems.java
@@ -1,12 +1,15 @@
 package syric.alchemine.setup;
 
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import syric.alchemine.Alchemine;
 import syric.alchemine.outputs.items.FullFlaskItem;
+import syric.alchemine.outputs.items.SlimeseedItem;
 
 public class AlchemineItems {
 
@@ -23,6 +26,17 @@ public class AlchemineItems {
             () -> new Item(new Item.Properties().tab(AlchemineCreativeTabs.ALCHEMY)));
 
 
+    //Products
+    public static final RegistryObject<Item> CHROMATIC_SLIME_ENHANCER = ITEMS.register("chroma_enhancer",
+            () -> new Item(new Item.Properties().tab(AlchemineCreativeTabs.ALCHEMICAL_CREATIONS)));
+    public static final RegistryObject<Item> CUSHIONING_SLIME_ENHANCER = ITEMS.register("cushioning_enhancer",
+            () -> new Item(new Item.Properties().tab(AlchemineCreativeTabs.ALCHEMICAL_CREATIONS)));
+    public static final RegistryObject<Item> BOUNCY_SLIME_ENHANCER = ITEMS.register("bouncy_enhancer",
+            () -> new Item(new Item.Properties().tab(AlchemineCreativeTabs.ALCHEMICAL_CREATIONS)));
+    public static final RegistryObject<Item> NONSTICK_SLIME_ENHANCER = ITEMS.register("slippery_enhancer",
+            () -> new Item(new Item.Properties().tab(AlchemineCreativeTabs.ALCHEMICAL_CREATIONS)));
+    public static final RegistryObject<Item> SLIMESEED = ITEMS.register("slimeseed",
+            () -> new SlimeseedItem(new Item.Properties().tab(AlchemineCreativeTabs.ALCHEMICAL_CREATIONS)));
 
     //INGREDIENTS (58 Items)
     public static final RegistryObject<Item> SALT = ITEMS.register("salt",
